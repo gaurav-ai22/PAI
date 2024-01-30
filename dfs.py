fc501801035aa8dbd4de5d4d27ab585edc339004
gaurav-ai22 <156395726+gaurav-ai22@users.noreply.github.com>
2024-01-30 20:01:24 +0530

    Create dfs.py

---
+++ dfs.py
@@ -0,0 +1,24 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next_node in graph[start] - visited:
+        dfs(graph, next_node, visited)
+    return visited
+
+# Example graph representation using dictionary
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+# Starting node for DFS traversal
+start_node = 'A'
+
+print("DFS traversal:")
+dfs(graph, start_node)
